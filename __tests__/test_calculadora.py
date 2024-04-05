5dfb2cc2d9169bef11bf083dda0277059a070074
Aline Lenzi <alinelenzi@gmail.com>
2024-04-05 19:07:49 -0300

    first commit

---
+++ __tests__/test_calculadora.py
@@ -0,0 +1,53 @@
+# bibliotecas, frameworks e referencias externas
+import pytest
+from calculadora import somar_dois_numeros, subtrair_dois_numeros, multiplicar_dois_numeros, dividir_dois_numeros
+
+# testes
+#Padrão /Standard AAA (se diz tiple A/ 3A) = Arrange(Prepara, Configura - Dados de entrada e saída), Act(executa), Assert(Valida)
+
+def test_somar_dois_numeros():
+    #Preparar
+    num1 = 5;
+    num2 = 7;
+    resultado_esperado = 12;
+
+    #Executar
+    resultado_obtido = somar_dois_numeros(num1, num2);
+
+    #Validar
+    assert resultado_esperado == resultado_obtido
+
+def test_subtrair_dois_numeros():
+    num1 = 7;
+    num2 = 5;
+    resultado_esperado = 2;
+
+    #Executar
+    resultado_obtido = subtrair_dois_numeros(num1, num2);
+
+    #Validar
+    assert resultado_esperado == resultado_obtido
+
+def test_multiplicar_dois_numeros():
+    num1 = 5;
+    num2 = 7;
+    resultado_esperado = 35;
+
+    #Executar
+    resultado_obtido = multiplicar_dois_numeros(num1, num2);
+
+    #Validar
+    assert resultado_esperado == resultado_obtido
+
+def test_dividir_dois_numeros():
+    num1 = 35;
+    num2 = 7;
+    resultado_esperado = 5;
+
+    #Executar
+    resultado_obtido = dividir_dois_numeros(num1, num2);
+
+    #Validar
+    assert resultado_esperado == resultado_obtido
+
+
